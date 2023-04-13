fd2690636d595113f604cbd2b6e2fad00edc3de7
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-04-13 20:39:13 -0300

    Add project files.

---
+++ tabuada-simples/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace tabuada_simples
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        double numero;
+
+        Console.WriteLine("Digite Um Número da Tabuada: ");
+
+        numero = double.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            double total = numero * i;
+            Console.WriteLine(numero + " x " + i + " = " + total);
+        }
+        Console.WriteLine("Criano uma tabuada simples no c#!");
+
+    }
+}
+}
